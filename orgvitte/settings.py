1ce77d40421690d2924593b8db2de9355eee9f47
andrey8309 <andrey.norsi@gmail.com>
2025-08-31 22:56:27 +0300

    Для Render2

--- orgvitte/settings.py
+++ orgvitte/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k5o6%8d_&!y7et2hk%8f_-62eb5jt(c@r0$egw68l6sy-t*!f1
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', '1') == '1'
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost, orgvitte2.onrender.com').split(',')
 
 
 
